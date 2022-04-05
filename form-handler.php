ddd4564bee9e9ab4950b0896c4363ad0b103d541
EarthyDevil <96943278+EarthyDevil@users.noreply.github.com>
2022-04-05 10:22:00 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
 
-    $email_from = 'info@yourwebsite.com';
+    $email_from = 'itzmegaming007@gmail.com';
 
     $email_subject = 'New Form Submission';
 
